19fde98fbb9079866ed906dbda7e748358d55f24
Pablo <p5.tacoronte@gmail.com>
2023-12-10 13:20:48 -0800

    header file

---
+++ ls/ls.h
@@ -0,0 +1,21 @@
+/*  ls Header File  */
+
+#ifndef LS_FILE_H
+#define LS_FILE_H
+
+
+/*  Libraries   */
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+
+
+/*  Functions   */
+
+//  pwdch checks if the string begins with "OLDPWD"
+int pwdch(char* str);
+
+//  getpath returns the current path
+char* getpath(char* str);
+
+#endif
