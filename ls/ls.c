f938ae42d3a875e1a7ab7c6b45749e43dcaa8591
Pablo <p5.tacoronte@gmail.com>
2023-12-11 20:25:28 -0800

    implemented list only one file

--- ls/ls.c
+++ ls/ls.c
@@ -14,71 +14,140 @@ int main(int ac, char** av, char** env)
 	int		pos = 0;
 	int		j = 0;
 	char*	path = NULL;
+	DIR*	directory = NULL;
 
 	/*	get the path -> pass to disdir	*/
 	if (ac == 1)
 	{
-		/*	get the current pwd	*/
-		while (env[j])
-		{
-			if (pwdch(env[j]))
-				pos = j;
-			j++;
-		}
-		/*	printf("%s\n", env[pos]);	*/
-		path = getpath(env[pos]);
-		/*	printf("%s\n", path);	*/
-		dirdis(path);
+		path = getpath(env);
+		directory = dirch(path);
+		dirdis(directory, NULL);
 	}
 	else
 	{
-		/*	read each path from av	*/
+		/*	read each path or filename from av	*/
 		for (pos = 1; pos < ac; pos++)
 		{
-			if (pos > 1)
-				printf("\n");
-			printf("%s:\n", av[pos]);
-			dirdis(av[pos]);
+			directory = dirch(av[pos]);
+			if (directory)
+				dirdis(directory, NULL);
+			else
+			{
+				path = getpath(env);
+				directory = dirch(path);
+				reclist(directory, av[pos]);
+			}
+			closedir(directory);
 		}
 	}
-
 	return (errno);
 }
 
 
 /**
 * dirdis- function
-* @path: int
+* @directory: DIR*
+* @filename: char*
 * Return: int
 */
-int dirdis(char* path)
+int dirdis(DIR*	directory, char* filename)
 {
-	DIR* directory = NULL;
-	struct	dirent* rd = NULL;
+	char*	errmsg = NULL;
+	struct	dirent*	rd = NULL;
 
-	/*	open -> read & print -> close stream	*/
-	directory = opendir(path);
+	/*	read & print -> close stream	*/
 	if (directory)
 	{
-		while ( (rd = readdir(directory)) )
+		if (!filename)
 		{
-			/*	printf("%lu\n" ,rd->d_ino);
-				printf("%lu\n" ,rd->d_off);
-				printf("%hu\n" ,rd->d_reclen);
-				printf("%d\n" ,rd->d_type);	*/
-			if (rd->d_name[0] != '.')
-				printf("%s\t", rd->d_name);
+			while ( (rd = readdir(directory)) )
+			{
+				//if (rd->d_type == 4)
+				//	printf("%s:\n", rd->d_name);
+				// printf("%lu\n" ,rd->d_ino);
+				// printf("%lu\n" ,rd->d_off);
+				// printf("%hu\n" ,rd->d_reclen);
+				// printf("%d\n" ,rd->d_type);
+				//if (rd->d_name[0] != '.')
+					printf("%s\t", rd->d_name);
+			}
+			printf("\n");
 		}
-		closedir(directory);
-		printf("\n");
+		// else
+		// {
+
+		// }
+	}
+	// else if ()
+	// {}
+	else
+	{
+		errmsg = strerror(errno);
+		printf("%s\n", errmsg);
 	}
 	return (errno);
 }
 
 
 /**
+* reclist- function
+* @directory: DIR*
+* @filename: char*
+* Return: int
+*/
+void reclist(DIR* directory, char* filename)
+{
+	struct	dirent*	rd = NULL;
+
+	rd = readdir(directory);
+	if (rd)
+	{
+		reclist(directory, filename);
+		if ((!filename) || (!strcmp(filename, rd->d_name)))
+			printf("%s\n", rd->d_name);
+	}
+	return;
+}
+
+
+/**
+* dirch- function
+* @path: char*
+* Return: DIR*
+*/
+DIR* dirch(char* path)
+{
+	DIR*	directory = NULL;
+
+	directory = opendir(path);
+	return (directory);
+}
+
+
+/**
+* getpath- function
+* @env: char**
+* Return: char*
+*/
+char* getpath(char** env)
+{
+	int		j = 0;
+	int		pos = 0;
+
+	/*	get the current pwd	*/
+		while (!pwdch(env[j]))
+			j++;
+		/*	printf("%s\n", env[j]);	*/
+	while (env[j][pos] != '=')
+		pos++;
+	/*	printf("%s\n", &env[j][pos + 1]);	*/
+	return (&(env[j][pos + 1]));
+}
+
+
+/**
 * pwdch- function
-* @str: int
+* @str: char*
 * Return: int
 */
 int pwdch(char* str)
@@ -94,19 +163,4 @@ int pwdch(char* str)
 		pos++;
 	}
 	return (flag);
-}
-
-
-/**
-* getpath- function
-* @str: int
-* Return: char*
-*/
-char* getpath(char* str)
-{
-	int		pos = 0;
-
-	while (str[pos] != '=')
-		pos++;
-	return (&(str[pos + 1]));
 }
