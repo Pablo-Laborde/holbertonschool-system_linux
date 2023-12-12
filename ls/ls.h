1434a1698d75cef11b48ad2976062371cdab490f
Pablo <p5.tacoronte@gmail.com>
2023-12-11 20:24:08 -0800

    new functions

--- ls/ls.h
+++ ls/ls.h
@@ -3,21 +3,30 @@
 
 
 /*	Libraries	*/
-#include <sys/types.h>
 #include <dirent.h>
-#include <stdio.h>
+
 #include <errno.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
 
 
 /*	Functions	*/
 
 /*	display files in the directory	*/
-int dirdis(char* path);
+int dirdis(DIR*	directory, char* filename);
 
-/*	pwdch checks if the string begins with "OLDPWD"	*/
-int pwdch(char* str);
+/*  lists directory recusively  */
+void reclist(DIR* directory, char* filename);
+
+/*  checks if path is a directory   */
+DIR* dirch(char* path);
 
 /*	getpath returns the current path	*/
-char* getpath(char* str);
+char* getpath(char** env);
+
+/*	pwdch checks if the string begins with "PWD"	*/
+int pwdch(char* str);
 
 #endif
