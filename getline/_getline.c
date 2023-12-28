4ca2f4dd003f3f13fb1cdfde14239055bb56147b
Pablo-Laborde <p5.tacoronte@gmail.com>
2023-12-28 03:09:00 -0300

    working on loop

--- getline/_getline.c
+++ getline/_getline.c
@@ -6,40 +6,59 @@
 * @fd: int
 * Return: char*
 */
-char *_getline(const int fd) {
+char *_getline(const int fd)
+{
 	static char
 		buff[READ_SIZE];
 	static int
-		pos = 0;
+		pos = 0,
+		flag = 1;
 	ssize_t
 		ar = 0;
 	int
 		i = pos,
-		len = 0,
-		flag = 1;
+		len = 0;
 	char
 		*str = NULL,
 		*aux = NULL;
 
 	(void)ar;
 	(void)aux;
-	if (flag) {
-		ar = read(fd, buff, READ_SIZE);
-		flag = 0;
-	}
-	if (buff[i]) {
-		while (buff[pos]) {
-			if (buff[pos] == '\n')
-				break;
-			pos++;
+	do {
+		if (flag)
+		{
+			ar = read(fd, buff, READ_SIZE);
+			flag = 0;
+			pos = 0;
+			i = 0;
+			aux = str;
 		}
-		len = pos - i + 1;
-		str = cpy_string(&buff[i], len);
-		if (str[len - 1] == '\n')
-			str[len - 1] = '\0';
-		if (buff[pos])
-			pos++;
-	}
+		if (buff[i])
+		{
+			while (buff[pos] && (pos < READ_SIZE))
+			{
+				if (buff[pos] == '\n')
+					break;
+				pos++;
+			}
+			if (pos == READ_SIZE)
+			{
+				pos--;
+				flag = 1;
+			}
+			len = pos - i + 1;
+			str = cpy_string(&buff[i], len);
+			if (str[len - 1] == '\n')
+				str[len - 1] = '\0';
+			if (buff[pos])
+				pos++;
+			if (aux)
+			{
+				str = join_strings(aux, str);
+				aux = NULL;
+			}
+		}
+	} while (flag);
 	return (str);
 }
 
@@ -50,7 +69,8 @@ char *_getline(const int fd) {
 * @str2: char*
 * Return: char*
 */
-char *join_strings(char *str1, char *str2) {
+char *join_strings(char *str1, char *str2)
+{
 	int
 		i = 0,
 		len1 = 0,
@@ -58,13 +78,15 @@ char *join_strings(char *str1, char *str2) {
 	char
 		*new_str = NULL;
 
-	if (str1 && str2) {
+	if (str1 && str2)
+	{
 		while (str1[len1])
 			len1++;
 		while (str2[len2])
 			len2++;
 		new_str = malloc(len1 + len2 + 1);
-		if (new_str) {
+		if (new_str)
+		{
 			for (; i < len1; i++)
 				new_str[i] = str1[i];
 			for (i = 0; i < len2; i++)
@@ -84,15 +106,18 @@ char *join_strings(char *str1, char *str2) {
 * @amt: int, doesn't take into account the '\0'
 * Return: char*
 */
-char *cpy_string(char *str, int amt) {
+char *cpy_string(char *str, int amt)
+{
 	int
 		i = 0;
 	char
 		*new_str = NULL;
 
-	if (str) {
+	if (str)
+	{
 		new_str = malloc(amt + 1);
-		if (new_str) {
+		if (new_str)
+		{
 			for (; i < amt; i++)
 				new_str[i] = str[i];
 			new_str[i] = '\0';
