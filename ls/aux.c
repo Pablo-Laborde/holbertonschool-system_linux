b1d35b674a17bd62454ec96cf0875dcf2b2acdc0
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-01-28 13:15:34 -0300

    betty

--- ls/aux.c
+++ ls/aux.c
@@ -31,3 +31,46 @@ void tosum(char **env, char *dirpath, char *filename, char *av, int mode)
 	}
 	closedir(directory);
 }
+
+
+/**
+* pwdch- function
+* @str: char*
+* Return: int
+*/
+int pwdch(char *str)
+{
+	int pos = 0, flag = 1;
+	char *name = "PWD";
+
+	while (name[pos])
+	{
+		if (str[pos] != name[pos])
+			flag *= 0;
+		pos++;
+	}
+	return (flag);
+}
+
+
+/**
+* strc- function
+* @str1: char*
+* @str2: char*
+* Return: int
+*/
+int strc(char *str1, char *str2)
+{
+	int i = 0, eq = 0;
+
+	if (str1 && str2)
+		do {
+			if (str1[i] != str2[i])
+				eq = 1;
+			i++;
+			if ((str1[i] && !str2[i]) || (!str1[i] && str2[i]))
+				eq = 1;
+		}	while (str1[i] && str2[i]);
+	return (eq);
+}
+
--- ls/ls.c
+++ ls/ls.c
@@ -105,7 +105,8 @@ char *folch(char *str, char **filename, int *mode)
 		}
 		if (slashpos > 0)
 		{
-			if ( (foldername = malloc(slashpos + 1)) )
+			foldername = malloc(slashpos + 1)
+			if (foldername)
 			{
 				for (j = 0; j < slashpos; j++)
 					foldername[j] = str[j];
@@ -150,47 +151,4 @@ char *getpath(char **env)
 		pos++;
 	/*	printf("%s\n", &env[j][pos + 1]);	*/
 	return (&(env[j][pos + 1]));
-}
-
-
-/**
-* pwdch- function
-* @str: char*
-* Return: int
-*/
-int pwdch(char *str)
-{
-	int pos = 0, flag = 1;
-	char *name = "PWD";
-
-	while (name[pos])
-	{
-		if (str[pos] != name[pos])
-			flag *= 0;
-		pos++;
-	}
-	return (flag);
-}
-
-
-/**
-* strc- function
-* @str1: char*
-* @str2: char*
-* Return: int
-*/
-int strc(char *str1, char *str2)
-{
-	int i = 0, eq = 0;
-
-	if (str1 && str2)
-		do
-		{
-			if (str1[i] != str2[i])
-				eq = 1;
-			i++;
-			if ((str1[i] && !str2[i]) || (!str1[i] && str2[i]))
-				eq = 1;
-		}	while (str1[i] && str2[i]);
-	return (eq);
 }
