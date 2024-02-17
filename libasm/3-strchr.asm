966c3c02a0ebc765fe42588cd1f68ec621368e28
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-02-17 16:09:15 -0300

    3

---
+++ libasm/3-strchr.asm
@@ -0,0 +1,28 @@
+BITS 64
+
+
+section .text
+	global asm_strchr
+
+asm_strchr:
+	mov		r8, 0h
+	mov		r9, rdi
+	mov		r10, rsi
+
+cmp_chars:
+	cmp		byte [r9 + r8], 0h
+	je		null
+	cmp		byte [r9 + r8], r10b
+	je		ret_add
+	inc		r8
+	jmp		cmp_chars
+
+	ret_add:
+		lea		rax, [r9 + r8]
+		jmp		return
+
+	null:
+		mov		rax, 0
+
+	return:
+		ret
