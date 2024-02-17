b39206c69ac73402be3860ea253bb4205825e4d5
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-02-17 12:31:13 -0300

    2-main.c

--- libasm/2-main.c
+++ libasm/2-main.c
@@ -5,9 +5,12 @@
 
 #include "libasm.h"
 
+#define N	strlen(S1)
+
 #define S1  "Holberton School"
 #define S2  ""
 #define S3  "Holberton Socool"
+#define S4	"Holberton"
 
 /**
  * main - Program entry point
@@ -18,7 +21,12 @@ int main(void)
 {
     assert(strncmp(S1, S3, 1) == asm_strncmp(S1, S3, 1));
     assert(strncmp(S1, S3, 11) == asm_strncmp(S1, S3, 11));
+		assert(strncmp(S1, S2, 11) == asm_strncmp(S1, S2, 11));
     assert(strncmp(S1, S3, 15) == asm_strncmp(S1, S3, 15));
+		assert(strncmp(S1, S4, 16) == asm_strncmp(S1, S4, 16));
+		assert(strncmp(S1, S2, N) == asm_strncmp(S1, S2, N));
+		assert(strncmp("Holberton School", "Holberton", strlen("Holberton School")) == asm_strncmp("Holberton School", "Holberton", strlen("Holberton School")));
+		assert(strncmp("Holberton", "Holberton School", strlen("Holberton School")) == asm_strncmp("Holberton", "Holberton School", strlen("Holberton School")));
 
     printf("All good!\n");
     return (EXIT_SUCCESS);
