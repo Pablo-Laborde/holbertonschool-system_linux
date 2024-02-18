95b7a325eb6d92dc24cd4deb46b561e81053b185
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-02-18 16:48:25 -0300

    6

--- libasm/6-putc.asm
+++ libasm/6-putc.asm
@@ -5,8 +5,11 @@ global asm_putc
 
 section .text
 	asm_putc:
-		mov		rax, 1		; write instruction
+		push	rdi
 		mov		rdx, 1		;	amount of char
-		mov		rsi, rdi	;	pointer to output
+		mov		rsi, rsp	;	pointer to output
 		mov		rdi, 1		; file destriptor to std_out
+		mov		rax, 1		; write instruction
+		syscall
+		pop		rdi
 		ret
