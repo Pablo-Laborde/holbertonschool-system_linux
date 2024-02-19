56526ed847b41550454a74b49a51eafbad83f54d
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-02-18 23:00:15 -0300

    7

---
+++ libasm/7-puts.asm
@@ -0,0 +1,27 @@
+BITS 64
+
+global asm_puts
+extern asm_strlen
+
+section .text
+	asm_puts:
+		lea r8, [rdi]
+		push	rbp
+		mov		rbp, rsp
+	
+		mov		rax, rdi
+		call	asm_strlen
+
+		mov		rsp, rbp
+		pop 	rbp
+
+		push r8
+
+		mov		rdx, rax		;	amount of char
+		mov		rsi, rsp	;	pointer to output
+		mov		rdi, 1		; file destriptor to std_out
+		mov		rax, 1		; write instruction
+		syscall
+		mov		rax, rdx
+		pop r8
+		ret
