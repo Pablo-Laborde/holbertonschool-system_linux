1211267996c44e7c46cf0d7a8b9076a10753c2c8
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-02-25 19:35:28 -0300

    5

---
+++ signals/5-signal_describe.c
@@ -0,0 +1,22 @@
+#include "signals.h"
+
+
+/**
+* main- func
+* Return: int
+*/
+int main(int ac, char **av)
+{
+	char *sigstr = NULL;
+
+	if (ac != 2)
+	{
+		printf("Usage: %s <signum>\n", av[0]);
+		return (EXIT_FAILURE);
+	}
+	sigstr = strsignal(atoi(av[1]));
+	if (!sigstr)
+		return (EXIT_FAILURE);
+	printf("%s: %s\n", av[1], sigstr);
+	return (EXIT_SUCCESS);
+}
