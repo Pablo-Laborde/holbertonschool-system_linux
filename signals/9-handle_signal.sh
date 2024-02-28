fa96c09b2cdf16b48e7cc37be21dc7fdf47ba79f
Pablo-Laborde <p5.tacoronte@gmail.com>
2024-02-27 23:01:36 -0300

    9

---
+++ signals/9-handle_signal.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Write a sh script that set a handler for the signals SIGABRT, SIGIO and SIGTERM
+	#The script must print Nope followed by a new line, each time a SIGABRT, SIGIO or SIGTERM is caught
+	#Your script must contain a shebang
+	#You’re not allowed to have more than 2 lines in your script
+	#You’re not allowed to have more than 1 instruction per line
