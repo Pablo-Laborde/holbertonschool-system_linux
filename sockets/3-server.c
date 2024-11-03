1f93cc843adbcc048d3b6e2e189bf4ddd5be7a84
Pablo Laborde <p5.tacoronte@gmail.com>
2024-11-03 17:56:59 -0300

    receiving message

---
+++ sockets/3-server.c
@@ -0,0 +1,41 @@
+#include "sockets.h"
+
+
+/**
+ * main- func
+ * Return: int
+ */
+int main(void)
+{
+	int s_fd = 0, c_fd = 0;
+	char buffer[256];
+	socklen_t len = 0;
+	struct sockaddr_in sa, ca;
+
+	s_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (s_fd == -1)
+		exit(errno);
+	sa.sin_family = AF_INET;
+	sa.sin_port = htons(12345);
+	sa.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (bind(s_fd, (struct sockaddr *)&sa, sizeof(struct sockaddr_in)) == -1)
+		exit(errno);
+	if (listen(s_fd, 1) == -1)
+		exit(errno);
+	printf("Server listening on port 12345\n");
+	len = (socklen_t)sizeof(struct sockaddr_in);
+	c_fd = accept(s_fd, (struct sockaddr *)&ca, &len);
+	if (c_fd == -1)
+		exit(errno);
+	printf("Client connected: %s\n", inet_ntoa(ca.sin_addr));
+	printf("Message received: \"");
+	memset(buffer, 0, 256);
+	while (recv(c_fd, buffer, 256, 0))
+	{
+		printf("%s", buffer);
+		memset(buffer, 0, 256);
+	}
+	printf("\"\n");
+	close(c_fd);
+	return (0);
+}
