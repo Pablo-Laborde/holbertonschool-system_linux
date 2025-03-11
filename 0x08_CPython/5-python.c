e5ffc7c48d202060c76c3e3f19ab0f58dc9993d3
Pablo Laborde <p5.tacoronte@gmail.com>
2025-03-10 21:46:56 -0300

    ex 5

--- 0x08_CPython/5-python.c
+++ 0x08_CPython/5-python.c
@@ -1,11 +1,63 @@
 #include <Python.h>
 
 
+/* Pre 3.12 version */
+/**
+ * print_python_int - func
+ * @p: PyObject
+ */
+void print_python_int(PyObject *p)
+{
+	char sign = 0;
+	Py_ssize_t i = 0, len = 0;
+	unsigned long int n = 0, pow = 0, prod = 0;
+	PyLongObject *lob = NULL;
+	digit *dig;
+
+	/* printf("[*] Python integer info\n"); */
+	if (!PyLong_Check(p))
+	{
+		/* printf("  [ERROR] Invalid List Object\n"); */
+		printf("Invalid Int Object\n");
+		return;
+	}
+	lob = (PyLongObject *)p;
+	len = lob->ob_size;
+	if (len < 0)
+	{
+		len *= -1;
+		sign = '-';
+	}
+	dig = (digit *)lob->ob_digit;
+	if (!len)
+		n = 0;
+	else
+	{
+		for (; i < len; i++)
+		{
+			pow = (1UL << (PyLong_SHIFT * i));
+			prod = (unsigned long)dig[i] * pow;
+			if ((dig[i] && (pow > ULLONG_MAX / dig[i])) ||
+				(prod > (ULONG_MAX - n)))
+			{
+				printf("C unsigned long int overflow\n");
+				return;
+			}
+			n += prod;
+		}
+	}
+	printf("%c%lu\n", sign, n);
+}
+
+
+
+/* 3.12-Later version */
 
 /**
  * print_python_int - func
  * @p: PyObject
  */
+/*
 void print_python_int(PyObject *p)
 {
 	char sign = 0;
@@ -16,10 +68,10 @@ void print_python_int(PyObject *p)
 	uintptr_t lv_tag = 0;
 	digit *dig;
 
-	/* printf("[*] Python integer info\n"); */
+	printf("[*] Python integer info\n");
 	if (!PyLong_Check(p))
 	{
-		/* printf("  [ERROR] Invalid List Object\n"); */
+		printf("  [ERROR] Invalid List Object\n");
 		printf("Invalid Int Object\n");
 		return;
 	}
@@ -49,3 +101,4 @@ void print_python_int(PyObject *p)
 	}
 	printf("%c%lu\n", sign, n);
 }
+*/
