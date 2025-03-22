2cb16e51fe6d807045521b860fa90509eeef4bee
Pablo Laborde <p5.tacoronte@gmail.com>
2025-03-22 14:27:48 -0300

    fix logic

--- readelf/aux_func_one.c
+++ readelf/aux_func_one.c
@@ -12,41 +12,42 @@ void p_osabi(data_t *data, int ibe)
 
 	(void) ibe;
 	/* OS/ABI */
-	switch (data->e_ident[EI_OSABI]) {
-		case ELFOSABI_SYSV:
-			str = "UNIX - System V";
-			break;
-		case ELFOSABI_HPUX:
-			str = "UNIX - HP-UX";
-			break;
-		case ELFOSABI_NETBSD:
-			str = "UNIX - NetBSD";
-			break;
-		case ELFOSABI_LINUX:
-			str = "UNIX - Linux";
-			break;
-		case ELFOSABI_SOLARIS:
-			str = "UNIX - Solaris";
-			break;
-		case ELFOSABI_IRIX:
-			str = "UNIX - IRIX";
-			break;
-		case ELFOSABI_FREEBSD:
-			str = "UNIX - FreeBSD";
-			break;
-		case ELFOSABI_TRU64:
-			str = "UNIX - TRU64";
-			break;
-		case ELFOSABI_ARM:
-			str = "UNIX - ARM";
-			break;
-		case ELFOSABI_STANDALONE:
-			str = "UNIX - Stand-alone (embedded)";
-			break;
-		default:
-			printf("  OS/ABI:                            <unknown: %x>\n",
-				data->e_ident[EI_OSABI]);
-			return;
+	switch (data->e_ident[EI_OSABI])
+	{
+	case ELFOSABI_SYSV:
+		str = "UNIX - System V";
+		break;
+	case ELFOSABI_HPUX:
+		str = "UNIX - HP-UX";
+		break;
+	case ELFOSABI_NETBSD:
+		str = "UNIX - NetBSD";
+		break;
+	case ELFOSABI_LINUX:
+		str = "UNIX - Linux";
+		break;
+	case ELFOSABI_SOLARIS:
+		str = "UNIX - Solaris";
+		break;
+	case ELFOSABI_IRIX:
+		str = "UNIX - IRIX";
+		break;
+	case ELFOSABI_FREEBSD:
+		str = "UNIX - FreeBSD";
+		break;
+	case ELFOSABI_TRU64:
+		str = "UNIX - TRU64";
+		break;
+	case ELFOSABI_ARM:
+		str = "UNIX - ARM";
+		break;
+	case ELFOSABI_STANDALONE:
+		str = "UNIX - Stand-alone (embedded)";
+		break;
+	default:
+		printf("  OS/ABI:                            <unknown: %x>\n",
+			data->e_ident[EI_OSABI]);
+		return;
 	}
 	printf("  OS/ABI:                            %s\n", str);
 }
@@ -60,24 +61,25 @@ void p_osabi(data_t *data, int ibe)
 void p_type(data_t *data, int ibe)
 {
 	char *str = NULL;
-	int type = (ibe == ELFDATA2MSB) ? bswap_16(data->e_type) : data->e_type;
+	int type = (ibe) ? bswap_16(data->e_type) : data->e_type;
 
-	switch (type) {
-		case ET_NONE:
-			str = "NONE (Unknown type)";
-			break;
-		case ET_REL:
-			str = "REL (Relocatable file)";
-			break;
-		case ET_EXEC:
-			str = "EXEC (Executable file)";
-			break;
-		case ET_DYN:
-			str = "DYN (Position-Independent Executable file)";
-			break;
-		case ET_CORE:
-			str = "CORE (Core file)";
-			break;
+	switch (type)
+	{
+	case ET_NONE:
+		str = "NONE (Unknown type)";
+		break;
+	case ET_REL:
+		str = "REL (Relocatable file)";
+		break;
+	case ET_EXEC:
+		str = "EXEC (Executable file)";
+		break;
+	case ET_DYN:
+		str = "DYN (Position-Independent Executable file)";
+		break;
+	case ET_CORE:
+		str = "CORE (Core file)";
+		break;
 	}
 	printf("  Type:                              %s\n", str);
 }
