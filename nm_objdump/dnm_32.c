dac8b80b2485bc9b67ed49667004a7f9009caf99
Pablo Laborde <p5.tacoronte@gmail.com>
2025-04-14 01:09:50 -0300

    no name filter

--- nm_objdump/dnm_32.c
+++ nm_objdump/dnm_32.c
@@ -21,7 +21,7 @@ int manage_sym32_list(int fd, data32_t *d, uint32_t size)
 		if (read(fd, &sym, sizeof(Elf32_Sym)) != sizeof(Elf32_Sym))
 			return (1);
 		pos += sizeof(Elf32_Sym);
-		if (!sym.st_value /*|| !sym.st_name*/)
+		if (!sym.st_value || !sym.st_name)
 			continue;
 		/*if (p_address(d, &sym) || p_type(fd, d, &sym) || p_name(fd, d, &sym))*/
 		if (p_all(fd, d, &sym))
