57ddac2d776edc993c4512e256cba52889ea0648
Pablo Laborde <p5.tacoronte@gmail.com>
2025-04-15 11:56:29 -0300

    print to stderr

--- ls/aux.c
+++ ls/aux.c
@@ -64,8 +64,8 @@ int manage_files(int ac, char **av, int flags)
 	for (; i < ac; i++)
 		if ((lstat(av[i], &ss) == -1) && ((av[i][0] != '-') || !av[i][1]))
 		{
-			printf("./hls: cannot access %s: No such file or directory\n",
-				av[i]);
+			fprintf(stderr,
+				"./hls: cannot access %s: No such file or directory\n",	av[i]);
 			printed |= 1;
 		}
 	for (i = 1; i < ac; i++)
