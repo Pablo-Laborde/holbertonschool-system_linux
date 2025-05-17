4831a5e02bdd03689dadca4cc6974cd453570d65
Pablo Laborde <p5.tacoronte@gmail.com>
2025-05-17 11:05:24 -0300

    3

--- multithreading/21-prime_factors.c
+++ multithreading/21-prime_factors.c
@@ -9,7 +9,7 @@
 */
 list_t *prime_factors(char const *s)
 {
-	unsigned long int n = 0, lim = 0, i = 0, *p = NULL;
+	unsigned long int n = 0, lim = 0, i = 2, *p = NULL;
 	list_t *list = NULL;
 
 	list = malloc(sizeof(list_t));
@@ -17,22 +17,29 @@ list_t *prime_factors(char const *s)
 		exit(1);
 	list_init(list);
 	n = strtol(s, NULL, 10);
-	lim = n / 2;
-	while ((i <= lim) && (i <= n))
+	lim = n;
+	if (n < 2)
 	{
-		if (i < 2)
-			i = 2;
-		if (!(n % i))
-		{
-			p = malloc(sizeof(unsigned long int));
-			if (!p)
-				exit(1);
-			*p = i;
-			list_add(list, (void *)p);
-			n /= i;
-		}
-		else
-			i++;
+		p = malloc(sizeof(unsigned long int));
+		if (!p)
+			exit(1);
+		*p = n;
+		list_add(list, (void *)p);
 	}
+	else
+		while (i <= lim)
+		{
+			if (!(n % i))
+			{
+				p = malloc(sizeof(unsigned long int));
+				if (!p)
+					exit(1);
+				*p = i;
+				list_add(list, (void *)p);
+				n /= i;
+			}
+			else
+				i++;
+		}
 	return (list);
 }
