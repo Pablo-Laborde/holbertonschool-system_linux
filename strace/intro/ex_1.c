910869b18f9ebb388dcfb61373011b7f5846e972
Pablo Laborde <p5.tacoronte@gmail.com>
2025-05-31 23:15:55 -0300

    1

---
+++ strace/intro/ex_1.c
@@ -0,0 +1,39 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ptrace.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(int ac, char **av, char **env)
+{
+	pid_t pid = 0;
+	int status = 0, i = 0;
+	char **cav = NULL;
+
+	cav = malloc(sizeof(char *) * ac);
+	if (!cav)
+		return (1);
+	for (; i < (ac - 1); i++)
+		cav[i] = av[i + 1];
+	cav[i] = NULL;
+	pid = fork();
+	if (!pid)
+	{
+		ptrace(PTRACE_TRACEME, pid, NULL, NULL);
+		execve(cav[0], cav, env);
+	}
+	else
+	{
+		while (waitpid(pid, &status, 0) && !WIFEXITED(status))
+		{
+			ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
+			ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
+			printf("syscall return\n");
+		}
+		printf("Exit status: %d\n", WEXITSTATUS(status));
+	}
+	free(cav);
+	return (0);
+}
