236108576fca720312b8638e02e3cdeb39eafc74
Pablo Laborde <p5.tacoronte@gmail.com>
2025-06-01 01:15:29 -0300

    0

---
+++ strace/strace_0.c
@@ -0,0 +1,51 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "syscall.h"
+
+
+
+/**
+* main - func
+* Return: int
+*/
+int main(int ac, char **av, char **env)
+{
+	pid_t pid = 0;
+	int status = 0, i = 0;
+	char **cav = NULL;
+	struct user_regs_struct data;
+
+	cav = malloc(sizeof(char *) * ac);
+	if (!cav)
+		return (1);
+	for (; i < (ac - 1); i++)
+		cav[i] = av[i + 1];
+	cav[i] = NULL;
+	pid = fork();
+	if (!pid)
+	{
+		ptrace(PTRACE_TRACEME, pid, NULL, NULL);
+		execve(cav[0], cav, env);
+	}
+	else
+	{
+		while (waitpid(pid, &status, 0) && !WIFEXITED(status))
+		{
+			ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
+			ptrace(PTRACE_GETREGS, pid, NULL, &data);
+			printf("%llu\n", data.rax);
+			ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
+		}
+		printf("Exit status: %d\n", WEXITSTATUS(status));
+	}
+	free(cav);
+	return (0);
+}
+
