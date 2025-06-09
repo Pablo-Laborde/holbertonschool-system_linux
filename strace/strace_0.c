1a952c3e95e684185bc18d98e51f5e211086edb2
Pablo Laborde <p5.tacoronte@gmail.com>
2025-06-08 23:23:20 -0300

    0

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -12,6 +12,27 @@
 
 
 /**
+* cp_av - func
+* @ac: int
+* @av: char **
+* Return: char **
+*/
+char **cp_av(int ac, char **av)
+{
+	char **cav = NULL;
+	int i = 0;
+
+	cav = malloc(sizeof(char *) * ac);
+	if (!cav)
+		exit(1);
+	for (; i < (ac - 1); i++)
+		cav[i] = av[i + 1];
+	cav[i] = NULL;
+	return (cav);
+}
+
+
+/**
 * main - func
 * @ac: int
 * @av: char **
@@ -21,32 +42,29 @@
 int main(int ac, char **av, char **env)
 {
 	pid_t pid = 0;
-	int status = 0, i = 0;
+	int status = 0;
 	char **cav = NULL;
 	struct user_regs_struct data;
 
-	cav = malloc(sizeof(char *) * ac);
-	if (!cav)
-		return (1);
-	for (; i < (ac - 1); i++)
-		cav[i] = av[i + 1];
-	cav[i] = NULL;
+	cav = cp_av(ac, av);
 	pid = fork();
 	if (!pid)
 	{
 		ptrace(PTRACE_TRACEME, pid, NULL, NULL);
 		execve(cav[0], cav, env);
 	}
-	else
+	waitpid(pid, &status, 0);
+	while (!WIFEXITED(status))
 	{
-		while (waitpid(pid, &status, 0) && !WIFEXITED(status))
-		{
-			ptrace(PTRACE_GETREGS, pid, NULL, &data);
-			if (data.orig_rax != -1)
-				printf("%llu\n", data.orig_rax);
-			ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
-			ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
-		}
+		ptrace(PTRACE_GETREGS, pid, NULL, &data);
+		if (data.orig_rax != -1)
+			printf("%llu\n", data.orig_rax);
+		/* Step to syscall exit */
+		ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
+		waitpid(pid, &status, 0);
+		/* Step to next syscall entry */
+		ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
+		waitpid(pid, &status, 0);
 	}
 	free(cav);
 	return (0);
