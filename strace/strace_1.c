a482bb717a65345ed7924179d68af3f9af34e910
Pablo Laborde <p5.tacoronte@gmail.com>
2025-06-13 23:01:11 -0300

    1

---
+++ strace/strace_1.c
@@ -0,0 +1,86 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "syscall.h"
+
+
+
+/**
+* cp_av - func
+* @ac: int
+* @av: char **
+* Return: char **
+*/
+char **cp_av(int ac, char **av)
+{
+	char **cav = NULL;
+	int i = 0;
+
+	cav = malloc(sizeof(char *) * ac);
+	if (!cav)
+		exit(1);
+	for (; i < (ac - 1); i++)
+		cav[i] = av[i + 1];
+	cav[i] = NULL;
+	return (cav);
+}
+
+
+/**
+* syscall_name - func
+* @no: unsigned long
+* Return: char *
+*/
+char *syscall_name(unsigned long no)
+{
+	return ((char *)((no == syscalls_64_g[no].nr) ?
+			syscalls_64_g[no].name : syscalls_32_g[no].name));
+}
+
+
+/**
+* main - func
+* @ac: int
+* @av: char **
+* @env: char **
+* Return: int
+*/
+int main(int ac, char **av, char **env)
+{
+	pid_t pid = 0;
+	int status = 0;
+	char **cav = NULL;
+	struct user_regs_struct data;
+
+	cav = cp_av(ac, av);
+	pid = fork();
+	if (!pid)
+	{
+		ptrace(PTRACE_TRACEME, pid, NULL, NULL);
+		execve(cav[0], cav, env);
+	}
+	waitpid(pid, &status, 0);
+	ptrace(PTRACE_GETREGS, pid, NULL, &data);
+	if ((long)data.orig_rax != -1)
+		printf("%s\n", syscall_name(data.orig_rax));
+	while (!WIFEXITED(status))
+	{
+		/* Step to syscall exit */
+		ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
+		waitpid(pid, &status, 0);
+		ptrace(PTRACE_GETREGS, pid, NULL, &data);
+		if ((long)data.orig_rax != -1)
+			printf("%s\n", syscall_name(data.orig_rax));
+		/* Step to next syscall entry */
+		ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
+		waitpid(pid, &status, 0);
+	}
+	free(cav);
+	return (0);
+}
